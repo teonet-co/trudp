60e7b06ff5d016c7d79df8dea5a671ceddb7b62f
Kirill Scherba <kirill@scherba.ru>
2016-07-01 19:28:51 +0300

    add project to the ci-build system

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -12,32 +12,32 @@ CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
 CND_ARTIFACT_NAME_Debug=trudpcat
 CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/trudpcat
 CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
-CND_PACKAGE_NAME_Debug=tr-udp.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/tr-udp.tar
+CND_PACKAGE_NAME_Debug=trudp.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/trudp.tar
 # Release configuration
 CND_PLATFORM_Release=GNU-Linux
 CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
-CND_ARTIFACT_NAME_Release=tr-udp
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/tr-udp
+CND_ARTIFACT_NAME_Release=trudp
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/trudp
 CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
-CND_PACKAGE_NAME_Release=tr-udp.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/tr-udp.tar
+CND_PACKAGE_NAME_Release=trudp.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/trudp.tar
 # MinGW-w64-Debug configuration
 CND_PLATFORM_MinGW-w64-Debug=GNU-MinGW-64-Linux
 CND_ARTIFACT_DIR_MinGW-w64-Debug=dist/MinGW-w64-Debug/GNU-MinGW-64-Linux
 CND_ARTIFACT_NAME_MinGW-w64-Debug=trudpcat
 CND_ARTIFACT_PATH_MinGW-w64-Debug=dist/MinGW-w64-Debug/GNU-MinGW-64-Linux/trudpcat
 CND_PACKAGE_DIR_MinGW-w64-Debug=dist/MinGW-w64-Debug/GNU-MinGW-64-Linux/package
-CND_PACKAGE_NAME_MinGW-w64-Debug=tr-udp.tar
-CND_PACKAGE_PATH_MinGW-w64-Debug=dist/MinGW-w64-Debug/GNU-MinGW-64-Linux/package/tr-udp.tar
+CND_PACKAGE_NAME_MinGW-w64-Debug=trudp.tar
+CND_PACKAGE_PATH_MinGW-w64-Debug=dist/MinGW-w64-Debug/GNU-MinGW-64-Linux/package/trudp.tar
 # MinGW-w64-Win32-Debug configuration
 CND_PLATFORM_MinGW-w64-Win32-Debug=GNU-MinGW-64-Win32-Linux
 CND_ARTIFACT_DIR_MinGW-w64-Win32-Debug=dist/MinGW-w64-Win32-Debug/GNU-MinGW-64-Win32-Linux
 CND_ARTIFACT_NAME_MinGW-w64-Win32-Debug=trudpcat.exe
 CND_ARTIFACT_PATH_MinGW-w64-Win32-Debug=dist/MinGW-w64-Win32-Debug/GNU-MinGW-64-Win32-Linux/trudpcat.exe
 CND_PACKAGE_DIR_MinGW-w64-Win32-Debug=dist/MinGW-w64-Win32-Debug/GNU-MinGW-64-Win32-Linux/package
-CND_PACKAGE_NAME_MinGW-w64-Win32-Debug=tr-udp.tar
-CND_PACKAGE_PATH_MinGW-w64-Win32-Debug=dist/MinGW-w64-Win32-Debug/GNU-MinGW-64-Win32-Linux/package/tr-udp.tar
+CND_PACKAGE_NAME_MinGW-w64-Win32-Debug=trudp.tar
+CND_PACKAGE_PATH_MinGW-w64-Win32-Debug=dist/MinGW-w64-Win32-Debug/GNU-MinGW-64-Win32-Linux/package/trudp.tar
 #
 # include compiler specific variables
 #
