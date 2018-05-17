23b90fc38beeee03ce75412175cdc0b588118ffa
Kirill Scherba <kirill@scherba.ru>
2018-05-17 14:36:01 +0300

    Build dependence added to the CircleCI configuration

--- ci-build-data/build-ubuntu.sh
+++ ci-build-data/build-ubuntu.sh
@@ -3,19 +3,19 @@
 # Set project dependence to make Teonet project from sources at empty host
 
 # Upgrade Ubuntu
-#sudo apt-get update
-#sudo apt-get -y upgrade
+sudo apt-get update
+sudo apt-get -y upgrade
 
 # Autoconf dependence
-#sudo apt-get -y install autoconf intltool libtool libglib2.0-dev doxygen make gcc g++ checkinstall
+sudo apt-get -y install autoconf intltool libtool libglib2.0-dev doxygen make gcc g++ checkinstall
 
 ## Project dependence
-#sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8CC88F3BE7D6113C
-#sudo apt-get install -y software-properties-common
-#sudo add-apt-repository "deb http://repo.ksproject.org/ubuntu/ teonet main"
-#sudo apt-get update
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 8CC88F3BE7D6113C
+sudo apt-get install -y software-properties-common
+sudo add-apt-repository "deb http://repo.ksproject.org/ubuntu/ teonet main"
+sudo apt-get update
 ##
-#sudo apt-get install -y libteonet-dev
+sudo apt-get install -y libteonet-dev
 
 ## Update system dynamic libraries configuration
-#sudo ldconfig
+sudo ldconfig
