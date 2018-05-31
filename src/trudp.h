9d2031be407a33cc75c3fdb6c47343acead223bd
max <mpano91@gmail.com>
2018-05-31 19:53:48 +0300

    Update teoccl

--- src/trudp.h
+++ src/trudp.h
@@ -196,7 +196,7 @@ typedef struct trudpStatData {
 typedef struct trudpData {
 
     uint32_t trudp_data_label[2]; ///< Labele to distinguish trudpData and trudpChannelData
-    teoMapData *map; ///< Channels map (key: ip:port:channel)
+    teoMap *map; ///< Channels map (key: ip:port:channel)
     void* psq_data; ///< Send queue process data (used in external event loop)
     void* user_data; ///< User data
     int port; ///< Port
