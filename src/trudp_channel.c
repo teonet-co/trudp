d3e63955817100bf173a6596bd7fcdc0762fb241
Anton Vasiliev <si1ver1502@gmail.com>
2019-11-28 19:50:45 +0300

    Zero memory for new trudpChannelData. Remove unused fields.

--- src/trudp_channel.c
+++ src/trudp_channel.c
@@ -113,10 +113,6 @@ static void _trudpChannelSetDefaults(trudpChannelData *tcd) {
   tcd->lastReceived = teoGetTimestampFull();
   tcd->lastSentPing = 0;  //  Never sent ping before
   tcd->triptimeMiddle = START_MIDDLE_TIME;
-  tcd->read_buffer = NULL;
-  tcd->read_buffer_ptr = 0;
-  tcd->read_buffer_size = 0;
-  tcd->last_packet_ptr = 0;
 
   // Initialize statistic
   trudpStatChannelInit(tcd);
@@ -131,11 +127,6 @@ static void _trudpChannelFree(trudpChannelData *tcd) {
 
   TD(tcd)->stat.sendQueue.size_current -= trudpSendQueueSize(tcd->sendQueue);
   TD(tcd)->stat.writeQueue.size_current -= trudpWriteQueueSize(tcd->writeQueue);
-
-  if (tcd->read_buffer != NULL) {
-    free(tcd->read_buffer);
-    tcd->read_buffer = NULL;
-  }
 
   trudpSendQueueFree(tcd->sendQueue);
   trudpWriteQueueFree(tcd->writeQueue);
@@ -158,6 +149,7 @@ trudpChannelData *trudpChannelNew(void *parent, char *remote_address,
                                   int remote_port_i, int channel) {
 
   trudpChannelData tcd;
+  memset(&tcd, 0, sizeof(tcd));
 
   tcd.td = parent;
   tcd.sendQueue = trudpSendQueueNew();
@@ -167,6 +159,8 @@ trudpChannelData *trudpChannelNew(void *parent, char *remote_address,
   trudpUdpMakeAddr(remote_address, remote_port_i, (__SOCKADDR_ARG)&tcd.remaddr,
                    &tcd.addrlen);
   tcd.channel = channel;
+
+  tcd.connected_f = 0;
 
   // Set other defaults
   _trudpChannelSetDefaults(&tcd);
